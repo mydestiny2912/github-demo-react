74ef41b227528390ab080ba0873da49690b602d0
Raymond Le <raymond.le@nexcel.vn>
2025-01-19 15:39:44 +0700

    init

---
+++ mydestiny-2912/pizza-menu/src/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>My Pizza dasdMenu !!!!</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
